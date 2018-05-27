e5cda427230838583bf13661792fb2d3ea635918
Evan Stroud <evan.stroud@wework.com>
2018-05-27 14:43:07 +0000

    Done.

---
+++ hello_world.c
@@ -0,0 +1,5 @@
+![Hello World! Art](https://d32dm0rphc51dk.cloudfront.net/b6JQ66-0nHij79irJT-Pdg/large.jpg)
+
+_[Hello World! by Brian Kernighan, from Artsy's Algorythm Auction](https://www.artsy.net/artwork/brian-kernighan-hello-world) based on a 1974 Bell Laboratories internal memorandum by Brian Kernighan, Programming in C: A Tutorial, which contains the first known version._
+
+<p class='util--hide'>View <a href='https://learn.co/lessons/hello-world-ruby'>Hello World</a> on Learn.co and start learning to code for free.</p>
